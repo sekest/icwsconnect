333011ca3db97c5fe04bbfab227b4b738eb1f2e9
Seth <41645007+sekest@users.noreply.github.com>
2020-09-11 11:17:10 -0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,11 @@
+define(function(){
+    let params = {
+        //PARAM 1:
+        url : "https://acme.com/api/sessionmanager",
+        //PARAM 2:
+        username : "user",
+        //PARAM 3:
+        password : "password"
+    }
+	return params
+});
